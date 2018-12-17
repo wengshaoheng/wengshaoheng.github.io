dbb8e3c705825d4d0afb6d600f0efbc4ab5e2115
wengshaoheng <wengshaoheng@0easy.com>
2018-12-17 14:12:05 +0800

    update

---
+++ blog_list.js
@@ -0,0 +1,47 @@
+var blogList = [{
+        "catalog": "LINUX",
+        "createdDate": "yyyy-mm-dd",
+        "articleList": [{
+                "category"    : "SHELL",
+                "createdDate" : "yyyy-mm-dd",
+                "title"       : "Awk Script",
+                "url"         : "/#"
+            },{
+                "category"    : "COMMAND",
+                "createdDate" : "yyyy-mm-dd",
+                "title"       : "Grep Command",
+                "url"         : "/#"
+            }
+        ]
+    },{
+        "catalog": "JAVA",
+        "createdDate": "yyyy-mm-dd",
+        "articleList": [{
+                "category"    : "SPRING",
+                "createdDate" : "yyyy-mm-dd",
+                "title"       : "Spring AOP",
+                "url"         : "/#"
+            },{
+                "category"    : "SPRINGBOOT",
+                "createdDate" : "yyyy-mm-dd",
+                "title"       : "SpringBoot App",
+                "url"         : "/#"
+            }
+        ]
+    },{
+        "catalog": "DEVOPS",
+        "createdDate": "yyyy-mm-dd",
+        "articleList": [{
+                "category"    : "DOCKER",
+                "createdDate" : "yyyy-mm-dd",
+                "title"       : "How to build docker",
+                "url"         : "/#"
+            },{
+                "category"    : "KUBERNETES",
+                "createdDate" : "yyyy-mm-dd",
+                "title"       : "How to install K8S",
+                "url"         : "/#"
+            }
+        ]
+    }
+];
