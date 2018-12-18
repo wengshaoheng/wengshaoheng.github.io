da564d20fa36255ca8535288fcf1ac8984d06ccb
wengshaoheng <wengshaoheng@0easy.com>
2018-12-18 16:15:19 +0800

    update

--- blog_list.js
+++ blog_list.js
@@ -1,71 +1,71 @@
 var blogList = [{
-        "catalog": "LINUX",
+        "catalog": "Linux 工作笔记",
         "createdDate": "yyyy-mm-dd",
         "articleList": [{
                 "category"    : "SHELL",
                 "createdDate" : "yyyy-mm-dd",
                 "title"       : "Awk Script",
-                "url"         : "/#"
+                "url"         : "#"
             },{
                 "category"    : "COMMAND",
                 "createdDate" : "yyyy-mm-dd",
                 "title"       : "Grep Command",
-                "url"         : "/#"
+                "url"         : "#"
             }
         ]
     },{
-        "catalog": "JAVA",
+        "catalog": "Java 工作笔记",
         "createdDate": "yyyy-mm-dd",
         "articleList": [{
                 "category"    : "SPRING",
                 "createdDate" : "yyyy-mm-dd",
                 "title"       : "Spring AOP",
-                "url"         : "/#"
+                "url"         : "#"
             },{
                 "category"    : "SPRINGBOOT",
                 "createdDate" : "yyyy-mm-dd",
                 "title"       : "SpringBoot App",
-                "url"         : "/#"
+                "url"         : "#"
             }
         ]
     },{
-        "catalog": "DEVOPS",
+        "catalog": "DevOps 工作笔记",
         "createdDate": "yyyy-mm-dd",
         "articleList": [{
                 "category"    : "DOCKER",
                 "createdDate" : "yyyy-mm-dd",
                 "title"       : "How to build docker",
-                "url"         : "/#"
+                "url"         : "#"
             },{
                 "category"    : "KUBERNETES",
                 "createdDate" : "yyyy-mm-dd",
                 "title"       : "How to install K8S",
-                "url"         : "/#"
+                "url"         : "#"
             }
         ]
     },{
-        "catalog": "GIT",
+        "catalog": "Git 工作笔记",
         "createdDate": "yyyy-mm-dd",
         "articleList": [{
                 "category"    : "GIT",
                 "createdDate" : "yyyy-mm-dd",
                 "title"       : "GIT BASIC COMMANDS",
-                "url"         : "/#"
+                "url"         : "#"
             },{
                 "category"    : "GIT",
                 "createdDate" : "yyyy-mm-dd",
                 "title"       : "GIT ADVANCED USAGE ",
-                "url"         : "/#"
+                "url"         : "#"
             }
         ]
     },{
-        "catalog": "VUE",
+        "catalog": "Vue 学习笔记",
         "createdDate": "yyyy-mm-dd",
         "articleList": [{
                 "category"    : "VUE",
                 "createdDate" : "yyyy-mm-dd",
                 "title"       : "VUE DEMO",
-                "url"         : "/#"
+                "url"         : "#"
             }
         ]
     }
