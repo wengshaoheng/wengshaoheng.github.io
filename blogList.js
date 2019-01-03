97db438edeed532679538c54a9034a4f7dd8eaa2
wengshaoheng <wengshaoheng@0easy.com>
2019-01-03 18:13:53 +0800

    update

--- blogList.js
+++ blogList.js
@@ -1,187 +1,272 @@
-var blogList = [{
-        "catalog": "Linux 工作笔记",
-        "createdDate": "2018-12-25",
-        "articleList": [{
-                "category"    : "SHELL",
-                "createdDate" : "2018-12-25",
-                "title"       : "Awk Script",
-                "url"         : "#"
-            },{
-                "category"    : "COMMAND",
-                "createdDate" : "2018-12-25",
-                "title"       : "Grep Command",
-                "url"         : "#"
-            }
-        ]
-    },{
-        "catalog": "Windows 工作笔记",
-        "createdDate": "2018-12-25",
-        "articleList": [{
-                "category"    : "SPRING",
-                "createdDate" : "2018-12-25",
-                "title"       : "Spring AOP",
-                "url"         : "#"
-            },{
-                "category"    : "SPRINGBOOT",
-                "createdDate" : "2018-12-25",
-                "title"       : "SpringBoot App",
-                "url"         : "#"
-            }
-        ]
-    },{
-        "catalog": "Spring Boot 工作笔记",
-        "createdDate": "2018-12-25",
-        "articleList": [{
-                "category"    : "SPRING",
-                "createdDate" : "2018-12-25",
-                "title"       : "Spring AOP",
-                "url"         : "#"
-            },{
-                "category"    : "SPRINGBOOT",
-                "createdDate" : "2018-12-25",
-                "title"       : "SpringBoot App",
-                "url"         : "#"
-            }
-        ]
-    },{
-        "catalog": "Spring MVC 工作笔记",
-        "createdDate": "2018-12-25",
-        "articleList": [{
-                "category"    : "SPRING",
-                "createdDate" : "2018-12-25",
-                "title"       : "Spring AOP",
-                "url"         : "#"
-            },{
-                "category"    : "SPRINGBOOT",
-                "createdDate" : "2018-12-25",
-                "title"       : "SpringBoot App",
-                "url"         : "#"
-            }
-        ]
-    },{
-        "catalog": "Maven 工作笔记",
-        "createdDate": "2018-12-25",
-        "articleList": [{
-                "category"    : "SPRING",
-                "createdDate" : "2018-12-25",
-                "title"       : "Spring AOP",
-                "url"         : "#"
-            },{
-                "category"    : "SPRINGBOOT",
-                "createdDate" : "2018-12-25",
-                "title"       : "SpringBoot App",
-                "url"         : "#"
-            }
-        ]
-    },{
-        "catalog": "Spring Cloud 工作笔记",
-        "createdDate": "2018-12-25",
-        "articleList": [{
-                "category"    : "SPRING",
-                "createdDate" : "2018-12-25",
-                "title"       : "Spring AOP",
-                "url"         : "#"
-            },{
-                "category"    : "SPRINGBOOT",
-                "createdDate" : "2018-12-25",
-                "title"       : "SpringBoot App",
-                "url"         : "#"
-            }
-        ]
-    },{
-        "catalog": "MyBatis 工作笔记",
-        "createdDate": "2018-12-25",
-        "articleList": [{
-                "category"    : "SPRING",
-                "createdDate" : "2018-12-25",
-                "title"       : "Spring AOP",
-                "url"         : "#"
-            },{
-                "category"    : "SPRINGBOOT",
-                "createdDate" : "2018-12-25",
-                "title"       : "SpringBoot App",
-                "url"         : "#"
-            }
-        ]
-    },{
-        "catalog": "Docker 工作笔记",
-        "createdDate": "2018-12-25",
-        "articleList": [{
-                "category"    : "DOCKER",
-                "createdDate" : "2018-12-25",
-                "title"       : "How to build docker",
-                "url"         : "#"
-            },{
-                "category"    : "KUBERNETES",
-                "createdDate" : "2018-12-25",
-                "title"       : "How to install K8S",
-                "url"         : "#"
-            }
-        ]
-    },{
-        "catalog": "Kubernetes 工作笔记",
-        "createdDate": "2018-12-25",
-        "articleList": [{
-                "category"    : "DOCKER",
-                "createdDate" : "2018-12-25",
-                "title"       : "How to build docker",
-                "url"         : "#"
-            },{
-                "category"    : "KUBERNETES",
-                "createdDate" : "2018-12-25",
-                "title"       : "How to install K8S",
-                "url"         : "#"
-            }
-        ]
-    },{
-        "catalog": "Service Mesh 工作笔记",
-        "createdDate": "2018-12-25",
-        "articleList": [{
-                "category"    : "DOCKER",
-                "createdDate" : "2018-12-25",
-                "title"       : "How to build docker",
-                "url"         : "#"
-            },{
-                "category"    : "KUBERNETES",
-                "createdDate" : "2018-12-25",
-                "title"       : "How to install K8S",
-                "url"         : "#"
-            }
-        ]
-    },{
-        "catalog": "Git 工作笔记",
-        "createdDate": "2018-12-25",
-        "articleList": [{
-                "category"    : "GIT",
-                "createdDate" : "2018-12-25",
-                "title"       : "GIT BASIC COMMANDS",
-                "url"         : "#"
-            },{
-                "category"    : "GIT",
-                "createdDate" : "2018-12-25",
-                "title"       : "GIT ADVANCED USAGE ",
-                "url"         : "#"
-            }
-        ]
-    },{
-        "catalog": "Vue 学习笔记",
-        "createdDate": "2018-12-25",
-        "articleList": [{
-                "category"    : "VUE",
-                "createdDate" : "2018-12-25",
-                "title"       : "VUE DEMO",
-                "url"         : "#"
-            }
-        ]
-    },{
-        "catalog": "JQuery 学习笔记",
-        "createdDate": "2018-12-25",
-        "articleList": [{
-                "category"    : "VUE",
-                "createdDate" : "2018-12-25",
-                "title"       : "VUE DEMO",
-                "url"         : "#"
-            }
-        ]
-    }
+var blogList = [
+	{
+		"catalog" : "Git",
+		"articleList" : [
+			{
+				"category" : "GIT",
+				"articles" : [
+					{
+						"title" : "xxxx",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*GIT END*/
+	{
+		"catalog" : "Maven",
+		"articleList" : [
+			{
+				"category" : "MAVEN",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*MAVEN END*/
+	{
+		"catalog" : "Java",
+		"articleList" : [
+			{
+				"category" : "JAVA",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*JAVA END*/
+	{
+		"catalog" : "Spring",
+		"articleList" : [
+			{
+				"category" : "SPRING",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*SPRING END*/
+	{
+		"catalog" : "Spring MVC",
+		"articleList" : [
+			{
+				"category" : "SPRINGMVC",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*SPRINGMVC END*/
+	{
+		"catalog" : "Spring Boot",
+		"articleList" : [
+			{
+				"category" : "SPRINGBOOT",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*SPRINGBOOT END*/
+	{
+		"catalog" : "Spring Cloud",
+		"articleList" : [
+			{
+				"category" : "SPRINGCLOUD",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*SPRINGCLOUD END*/
+	{
+		"catalog" : "Mybatis",
+		"articleList" : [
+			{
+				"category" : "MYBATIS",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*MYBATIS END*/
+	{
+		"catalog" : "Vue",
+		"articleList" : [
+			{
+				"category" : "VUE",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*VUE END*/
+	{
+		"catalog" : "JQuery",
+		"articleList" : [
+			{
+				"category" : "JQUERY",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*JQUERY END*/
+	{
+		"catalog" : "Linux",
+		"articleList" : [
+			{
+				"category" : "LINUX",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*LINUX END*/
+	{
+		"catalog" : "Python",
+		"articleList" : [
+			{
+				"category" : "PYTHON",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*PYTHON END*/
+	{
+		"catalog" : "Gloang",
+		"articleList" : [
+			{
+				"category" : "GLOANG",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*GOLOANG END*/
+	{
+		"catalog" : "Html",
+		"articleList" : [
+			{
+				"category" : "HTML",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*HTML END*/
+	{
+		"catalog" : "Erlang",
+		"articleList" : [
+			{
+				"category" : "ERLANG",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*ERLANG END*/
+	{
+		"catalog" : "Docker",
+		"articleList" : [
+			{
+				"category" : "DOCKER",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	},  /*END DOCKER*/
+	{
+		"catalog" : "Kubernetes",
+		"articleList" : [
+			{
+				"category" : "KUBERNETES",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	}, /*END KUBERNETES*/
+	{
+		"catalog" : "DevOps",
+		"articleList" : [
+			{
+				"category" : "DEVOPS",
+				"articles" : [
+					{
+						"title" : "",
+						"createdDate" : "20190103",
+						"articleId" : "#"
+					}
+				]
+			}
+		]
+	} /*DEVOPS END*/
 ];
